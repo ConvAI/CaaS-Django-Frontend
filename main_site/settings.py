ba63b84fd59fd9acb1b6d8a14d76466d3c7da15c
Kuldip Patel <patel.kuldip91@gmail.com>
2020-02-29 15:53:11 +0530

    Copy to clipboard feature

--- main_site/settings.py
+++ main_site/settings.py
@@ -81,8 +81,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'nervaidb',
-        'USER': 'admin',
-        'PASSWORD': 'test',
+        'USER': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
         'PORT': '3306',
     }
